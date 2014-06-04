81d4ed2703b52dca11af83c0c69941cd67f3eccb
Alumno <alumno@localhost.localdomain>
2014-06-04 08:23:20 -0600

    tarea con evaluacion

--- src/MiClase.java
+++ src/MiClase.java
@@ -4,42 +4,55 @@ public class MiClase
 	//devuelve la suma de a y b
 	static int sumarNumeros(int a, int b)
 	{
-		return -1;
+		return a+b;
 	}
 	
 	//devuelve la resta de a y b
 	static int restarNumeros(int a, int b)
 	{
-		return -1;
+		return a-b;
 	}
 	
 	//devuelve la multiplicación de a y b
 	static int multiplicarNumeros(int a, int b)
 	{
-		return -1;
+		return a*b;
 	}
 	
 	//devuelve true si num es par, de lo contrario, devuelve false
 	static boolean esPar(int num)
 	{
+		if (num%2==0){
+			return true;
+		}
 		return false;
 	}
 	
 	//devuelve el numero mayor entre a, b o c
 	static int getMayor(int a, int b, int c)
 	{
-		return -1;
+		if (a>b && a>c)
+			return a;
+		
+		if (b>a && b>c)
+			return b;
+		
+		if (c>b && c>a)
+			return c;
+		return a;
 	}
+		
 	
 	//devuelve num elevado a la potencia
 	static int potencia(int num, int potencia)
 	{
 		return -1;
+	
 	}
 	
 	//devuelve el numero mayor del arreglo
 	static int sumarArreglo(int arreglo[])
-	{
+	{	
 		return -1;
 	}
 	
@@ -52,17 +65,19 @@ public class MiClase
 	//devuelve el numero menor de un arreglo
 	static int getMenorArreglo(int arreglo[])
 	{
+		
 		return -1;
 	}
 	
 	//devuelve true si encuentra cadena dentro arreglo
 	static boolean existe(String arreglo[],String cadena)
 	{
+		
 		return false;
 	}
 	
 	public static void main(String args[])
 	{
-		
+
 	}
 }
